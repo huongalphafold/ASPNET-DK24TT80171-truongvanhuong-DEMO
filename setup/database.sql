ee37bb7413507509ba94d75abde894ea93dd6f02
TRƯƠNG VĂN HƯỚNG - 170124888 <huongalphafold@gmail.com>
2026-05-31 19:21:38 +0700

    Update database.sql

--- setup/database.sql
+++ setup/database.sql
@@ -8,19 +8,36 @@ CREATE TABLE ThiSinh (
     MaThiSinh INT IDENTITY(1,1) PRIMARY KEY,
     HoTen NVARCHAR(100),
     Email NVARCHAR(100),
-    SoDienThoai VARCHAR(20)
+    SoDienThoai NVARCHAR(20)
 );
 
 CREATE TABLE TacPham (
     MaTacPham INT IDENTITY(1,1) PRIMARY KEY,
-    TenTacPham NVARCHAR(100),
+    TenTacPham NVARCHAR(200),
     TacGia NVARCHAR(100),
-    HinhAnh NVARCHAR(255)
+    DuongDanAnh NVARCHAR(255),
+    NgayNop DATETIME DEFAULT GETDATE()
 );
 
 CREATE TABLE ChamDiem (
     MaChamDiem INT IDENTITY(1,1) PRIMARY KEY,
     MaTacPham INT,
-    Diem INT,
-    NhanXet NVARCHAR(500)
+    Diem FLOAT,
+    NhanXet NVARCHAR(500),
+    FOREIGN KEY (MaTacPham)
+    REFERENCES TacPham(MaTacPham)
 );
+
+INSERT INTO ThiSinh(HoTen,Email,SoDienThoai)
+VALUES
+(N'Trương Văn Hướng','huong@gmail.com','0901234567');
+
+INSERT INTO TacPham(TenTacPham,TacGia,DuongDanAnh)
+VALUES
+(N'Bình minh quê hương',
+N'Trương Văn Hướng',
+'Bieutuong.png');
+
+INSERT INTO ChamDiem(MaTacPham,Diem,NhanXet)
+VALUES
+(1,95,N'Tác phẩm đẹp, bố cục tốt');
